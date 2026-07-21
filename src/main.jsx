e8ffbae357509c0f15fdf83e79b35b2b30f11707
Aniket Chauhan <cvinesh70@gmail.com>
2026-07-21 22:59:06 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./index.css";
-import { Cursor, CursorProvider } from "@phazr/custom-cursor";
 
 createRoot(document.getElementById("root")).render(
-  <CursorProvider>
+  <StrictMode>
     <App />
-    <Cursor />
-  </CursorProvider>,
+  </StrictMode>,
 );
